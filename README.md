f70e5f53892bcbde747bf2dc0d2e6ebde43418ef
Niko Kortelainen <nikortel@gmail.com>
2015-12-08 18:54:28 +0200

    Adds tool chain for insanity project. Tool chain created via Yeoman yeogurt generator. Adds rot-js dependency and the example map generation code for testing the setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# insanity
+# insanity, toolchain via yeoman yeogurt generator, see more about folder structure: https://github.com/larsonjj/generator-yeogurt
+- Install node >=0.12 and gulp
+- execute 'npm install' in project root
+- execute 'gulp serve --open'
+- Develop
---
+++ src/_scripts/main.js
@@ -0,0 +1,56 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+import $ from 'jquery';
+import ROT from 'rot-js'
+
+var Game = {
+    display: null,
+    map: {},
+
+    init: function() {
+        this.display = new ROT.Display();
+        document.getElementById('game-container').appendChild(this.display.getContainer());
+
+        this._generateMap();
+    },
+
+    _generateMap: function() {
+        var digger = new ROT.Map.Digger();
+        var freeCells = [];
+
+        var digCallback = function(x, y, value) {
+            if (value) { return; }
+
+            var key = x+","+y;
+            this.map[key] = ".";
+            freeCells.push(key);
+        }
+        digger.create(digCallback.bind(this));
+
+        this._generateBoxes(freeCells);
+
+        this._drawWholeMap();
+    },
+
+    _generateBoxes: function(freeCells) {
+        for (var i=0;i<10;i++) {
+            var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+            var key = freeCells.splice(index, 1)[0];
+            this.map[key] = "*";
+        }
+    },
+
+    _drawWholeMap: function() {
+        for (var key in this.map) {
+            var parts = key.split(",");
+            var x = parseInt(parts[0]);
+            var y = parseInt(parts[1]);
+            this.display.draw(x, y, this.map[key]);
+        }
+    }
+};
+
+Game.init();
